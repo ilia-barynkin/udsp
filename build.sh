b02bfabe6220614c282f3ae160f54b20aac79166
ilia-barynkin <iliabarynkin@yandex.ru>
2023-11-30 16:05:54 +0400

    something's work

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/sh
-set -e
 
 rm -rf build
 mkdir build
@@ -7,4 +6,20 @@ cd build
 cmake ./..
 make -j$(nproc)
 make run_tests
-cd ..
+
+# This is an ad-hoc solution to check if the library works
+# without messing with the CMake / setuptools hell
+# TODO: replace it with a less barbaric solution
+
+mkdir functests
+cp ./lib/libudsp.so ./functests
+cp -r ../py_bindings/*.py ./functests
+mkdir ./functests/data
+cp ../tests/weight/inmove1.xlsm ./functests/data/inmove1.xlsm
+cp ../tests/weight/weight.csv ./functests/data/weight.csv
+cp ../tests/dist/dist.xlsx ./functests/data/dist.xlsx
+cp ../tests/dist/dist.csv ./functests/data/dist.csv
+cp -r ../tests/**/*.py ./functests
+cd functests
+
+cd ../..
